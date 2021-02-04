280885786a33255cf3ea2f401601b03576764a91
itzik yisaschar <itzik.yis@gmail.com>
2021-02-04 08:31:00 +0200

    Updates

---
+++ precache-manifest.148ff6f0f4e5850c6b81627b63702111.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f901caaf2b813861f1f57150956e0038",
+    "url": "/UsersManegment/index.html"
+  },
+  {
+    "revision": "42947294054a2cbde1d8",
+    "url": "/UsersManegment/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "35b243e45100c146be69",
+    "url": "/UsersManegment/static/css/main.88b58497.chunk.css"
+  },
+  {
+    "revision": "42947294054a2cbde1d8",
+    "url": "/UsersManegment/static/js/2.6cc854a7.chunk.js"
+  },
+  {
+    "revision": "2e54b551fdc3988dd651d638460fe789",
+    "url": "/UsersManegment/static/js/2.6cc854a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35b243e45100c146be69",
+    "url": "/UsersManegment/static/js/main.53f1e8e4.chunk.js"
+  },
+  {
+    "revision": "e8701302e4954cb4450e",
+    "url": "/UsersManegment/static/js/runtime-main.1cb43476.js"
+  }
+]);
